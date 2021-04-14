1fdf26b1da22e2d8bbc47954e700ac02f4becae3
KennethSkelton <48537790+KennethSkelton@users.noreply.github.com>
2021-04-14 02:59:00 -0400

    Delete item.js

--- item.js
+++
@@ -1,9 +0,0 @@
-let myclass = class MarketplaceItem{
-    constructor(ownerID, description, price){
-        this.ownerID=ownerID
-        this.description=description
-        this.price=price
-    }
-}
-
-export { myclass }
